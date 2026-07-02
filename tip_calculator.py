045352cfdbf95c8fa80210422fff694fb21a84f8
Amit Tripathi <amit5000tripathi@gmail.com>
2026-07-02 17:05:54 +0530

    added percentage tip

--- tip_calculator.py
+++ tip_calculator.py
@@ -2,9 +2,9 @@ print("Welcome to the tip calculator")
 
 
 bill = float(input("what was the total bill? :"))
-tip = float(input("How much tip would you like to give? 5, 10, 13 : "))
+tip = int(input("What percentage tip would you like to give? 5, 10, 13 : "))
 
-total_bill = bill+tip
+total_bill = round(bill+(bill/100)*tip,2)
 people = float(input("How many people to split the bill ? : "))
 
-print(f"Each person should pay : {round((total_bill/people),2)}")
+print(f"Each person should pay : {round((total_bill/people),2)}$")
