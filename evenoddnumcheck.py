e54f76e994104b04facb0b627fc6e3ae5aae7ee0
Amit Tripathi <amit5000tripathi@gmail.com>
2026-07-07 14:47:00 +0530

    added if else, modulo practice session

---
+++ evenoddnumcheck.py
@@ -0,0 +1,9 @@
+print(''' Welcome to Even & Odd number checker
+      ===========================================''')
+
+num = int(input("Enter a number : "))
+
+if num%2 == 1:
+    print(f"Hi {num} is a ODD number :)")
+else:
+    print(f"Hi {num} is an EVEN number :(")
---
+++ rollercoastre.py
@@ -0,0 +1,8 @@
+print("Welcome to the Rollercoaster!")
+
+height = int(input("ENter your height in CM : "))
+
+if height >= 120:
+    print("yayyyyyyyy! You Can RIDE the Rollercoaster")
+else:
+    print("Sorry you have to groww before you can ride :)")
