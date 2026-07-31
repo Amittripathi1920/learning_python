76ce29a8bebae6bd7cb56100f6f9617af9178676
Amit Tripathi <66103992+Amittripathi1920@users.noreply.github.com>
2026-07-31 05:16:34 +0000

    Starting day 13 and build number guessing game on day 12

---
+++ day_12/demo.py
@@ -0,0 +1,25 @@
+
+# enemies = 1
+
+# def increase_enemies():
+#     enemies = 4
+#     print(f"Inside - {enemies}")
+
+# increase_enemies()
+# print(f"Outside : {enemies}")
+
+# def is_prime(num):
+#     if num <= 1:
+#         return False
+
+#     for i in range(2, num):
+#         if num % i == 0:
+#             return False
+
+#     return True
+
+# u_input = int(input("Enter a number to check : "))
+
+# # Test
+# print(f"{u_input} - {is_prime(u_input)}")   # True
+# # print(is_prime(10))  # False
--- day_12/numberGuessingGame.py
+++ day_12/numberGuessingGame.py
@@ -1,4 +1,61 @@
+import random
 
-difficulty_level = input("Choose a difficulty level (easy, medium, hard): ").lower()
+continue_game = True
 
-if difficulty_level == "easy"
+def numberCheck(random_digit : int, u_input: int, max_attempts:int):
+    # global max_attempts
+    max_attempts = max_attempts
+    if random_digit < u_input:
+        max_attempts -= 1
+        return "Too High" 
+    elif random_digit > u_input:
+        max_attempts -= 1
+        return "Too Low"
+    else :
+        return "You Got it!"
+
+while continue_game:
+
+    difficulty_level = input("Choose a difficulty level (E = easy, M = medium, H = hard): ").lower()
+
+    if difficulty_level == "e":
+        max_attempts = 10
+    elif difficulty_level == "m":
+        max_attempts = 7
+    elif difficulty_level == "h":
+        max_attempts = 5
+    else :
+        print("Invalid Level selected, please Choose correct difficulty level (E = easy, M = medium, H = hard): ")
+        continue_game = False
+        break
+
+    
+    random_digit = random.randint(1,100)
+    for i in range(max_attempts):
+        print(f"You have {max_attempts} attempts remaining to guess the number")
+        u_input = int(input("Make a Guess: "))
+        # x = numberCheck(random_digit=random_digit, u_input=u_input, max_attempts=max_attempts)
+        print(random_digit)
+        if random_digit < u_input:
+            max_attempts -= 1
+            print("Too High") 
+        elif random_digit > u_input:
+            max_attempts -= 1
+            print( "Too Low")
+        else :
+            print( "You Got it! Congratulations Winner")  
+            break
+
+        if max_attempts == 1:
+            print("This is your last guess, Think Before Typing.................")
+        elif max_attempts == 0 and u_input != random_digit:
+            print("You Lost\nThank You")
+        # print(f"u_input: {u_input},\nAttempts Left: {max_attempts}\nCorrect Ans: {random_digit}")
+    u_choice = input("Would You Like to play again? Type [Y/N] only: ").lower()
+    if u_choice != 'y':
+        print("Thank You for Playing....")
+        continue_game = False
+    else:
+        print( '\n' * 20)
+
+
